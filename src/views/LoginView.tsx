f34d704cf502c68d123be56f1d2857d5d9b89e6c
Edson <EdsonP4100@gmail.com>
2025-08-06 18:34:04 -0700

    axios client

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,27 +1,21 @@
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import ErrorMessage from '../components/ErrorMessage';
-import type { RegisterForm } from '../types';
+import type { LoginForm } from '../types';
 
 export default function LoginView() {
-  const initialValues: RegisterForm = {
-    name: '',
+  const initialValues: LoginForm = {
     email: '',
-    handle: '',
     password: '',
-    password_confirmation: '',
   };
 
   const {
     register,
-    watch,
     handleSubmit,
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const password = watch('password');
-
-  const handleRegister = (formData: RegisterForm) => {
+  const handleLogin = (formData: LoginForm) => {
     console.log(formData);
   };
 
@@ -30,25 +24,10 @@ export default function LoginView() {
       <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
 
       <form
-        onSubmit={handleSubmit(handleRegister)}
+        onSubmit={handleSubmit(handleLogin)}
         className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+        noValidate
       >
-        <div className="grid grid-cols-1 space-y-3">
-          <label htmlFor="name" className="text-2xl text-slate-500">
-            Nombre
-          </label>
-          <input
-            id="name"
-            type="text"
-            placeholder="Tu Nombre"
-            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register('name', {
-              required: 'El nombre es obligatorio',
-            })}
-          />
-          {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
-        </div>
-
         <div className="grid grid-cols-1 space-y-3">
           <label htmlFor="email" className="text-2xl text-slate-500">
             E-mail
@@ -59,7 +38,7 @@ export default function LoginView() {
             placeholder="Email de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
             {...register('email', {
-              required: 'El email es obligatorio',
+              required: 'El Email es obligatorio',
               pattern: {
                 value: /\S+@\S+\.\S+/,
                 message: 'E-mail no válido',
@@ -68,25 +47,6 @@ export default function LoginView() {
           />
           {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </div>
-
-        <div className="grid grid-cols-1 space-y-3">
-          <label htmlFor="handle" className="text-2xl text-slate-500">
-            Handle
-          </label>
-          <input
-            id="handle"
-            type="text"
-            placeholder="Nombre de usuario: sin espacios"
-            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register('handle', {
-              required: 'El handle es obligatorio',
-            })}
-          />
-          {errors.handle && (
-            <ErrorMessage>{errors.handle.message}</ErrorMessage>
-          )}
-        </div>
-
         <div className="grid grid-cols-1 space-y-3">
           <label htmlFor="password" className="text-2xl text-slate-500">
             Password
@@ -97,11 +57,7 @@ export default function LoginView() {
             placeholder="Password de Registro"
             className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
             {...register('password', {
-              required: 'El password es obligatorio',
-              minLength: {
-                value: 8,
-                message: 'El password debe ser mínimo de 8 caracteres',
-              },
+              required: 'El Password es obligatorio',
             })}
           />
           {errors.password && (
@@ -109,33 +65,10 @@ export default function LoginView() {
           )}
         </div>
 
-        <div className="grid grid-cols-1 space-y-3">
-          <label
-            htmlFor="password_confirmation"
-            className="text-2xl text-slate-500"
-          >
-            Repetir Password
-          </label>
-          <input
-            id="password_confirmation"
-            type="password"
-            placeholder="Repetir Password"
-            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
-            {...register('password_confirmation', {
-              required: 'Repetir password es obligatorio',
-              validate: (value) =>
-                value === password || 'Los passwords no son iguales',
-            })}
-          />
-          {errors.password_confirmation && (
-            <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage>
-          )}
-        </div>
-
         <input
           type="submit"
           className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
-          value="Crear Cuenta"
+          value="Iniciar Sesión"
         />
       </form>
 
